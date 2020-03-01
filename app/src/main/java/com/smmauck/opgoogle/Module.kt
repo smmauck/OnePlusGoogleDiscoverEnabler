fd0fb61a5107ecc4b0f140ffc43819c21d8659fa
Sam Mauck <samuel.mauck@colorado.edu>
2020-03-01 13:33:56 -0700

    Update to work on launcher version 4.2

--- app/src/main/java/com/smmauck/opgoogle/Module.kt
+++ app/src/main/java/com/smmauck/opgoogle/Module.kt
@@ -1,22 +1,29 @@
 package com.smmauck.opgoogle
 
 import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
-import de.robv.android.xposed.XposedHelpers.findClass
-import de.robv.android.xposed.XposedHelpers.setStaticBooleanField
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
-class Module: IXposedHookLoadPackage {
+
+class Module : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: LoadPackageParam) {
         if (lpparam.packageName != "net.oneplus.launcher")
             return
 
         XposedBridge.log("[smmauck] Loaded app: " + lpparam.packageName)
 
-        setStaticBooleanField(
-            findClass("net.oneplus.launcher.config.FeatureFlags", lpparam.classLoader),
-            "CUSTOMIZE_LEFT_MOST_SCREEN_ENABLED",
-            true
+        XposedHelpers.findAndHookMethod(
+            "net.oneplus.launcher.config.SkuHelper",
+            lpparam.classLoader,
+            "showDiscover",
+            object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    param.result = true
+                }
+            }
         )
     }
 }
